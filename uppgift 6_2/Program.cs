2a681e14538c26a3b6cd50d212535a30b6e38f7e
ouo2u <111962660+ouo2u@users.noreply.github.com>
2023-02-10 10:27:49 +0100

    Update Program.cs

--- uppgift 6_2/Program.cs
+++ uppgift 6_2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace upggift6_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in vad tal1 ska bli");
+          
+           int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("skriv in vad tal2 ska bli");
+          
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"det störta talet blir: {störst(tal1,tal2)}");
+
+        }
+        static int störst(int tal1, int tal2)
+        {
+          
+            if (tal1 == tal2)
+            {
+                return tal1;
+            }
+            else if (tal2 > tal1)
+            {
+                return tal2;
+            }
+            else
+            {
+                return tal1;
+            }
+        }
+    }
+}
